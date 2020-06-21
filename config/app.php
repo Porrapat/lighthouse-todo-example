dac5efa2af930b0a1754cac9005ed2baed524318
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2020-06-21 21:48:23 +0700

    Upgrade ligthouse version to use current and make it runable first.

--- config/app.php
+++ config/app.php
@@ -148,11 +148,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
-         */
-        Nuwave\Lighthouse\Providers\LighthouseServiceProvider::class,
-
-        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
